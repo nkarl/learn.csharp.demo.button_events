9ac0a03df0b84ccb940d6436f5c03c7c39fa17dd
nkarl <21993921+nkarl@users.noreply.github.com>
2022-02-14 16:52:50 -0800

    created this demo

---
+++ Demo_ButtonEvents/Form1.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="Form1.cs" company="Carles Norden -- 011606177">
+// Copyright (c) Carles Norden -- 011606177. All rights reserved.
+// </copyright>
+
+namespace Demo_ButtonEvents
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The Windows Form Application.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
